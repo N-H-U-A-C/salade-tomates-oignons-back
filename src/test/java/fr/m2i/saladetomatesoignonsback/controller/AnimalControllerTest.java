92b4796b8f96a3eda0f7d668ff39c9d5bd96dc02
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-09-16 19:27:12 +0200

    test: write unit tests for AnimalController

---
+++ src/test/java/fr/m2i/saladetomatesoignonsback/controller/AnimalControllerTest.java
@@ -0,0 +1,117 @@
+package fr.m2i.saladetomatesoignonsback.controller;
+
+import fr.m2i.saladetomatesoignonsback.business.service.AnimalService;
+import fr.m2i.saladetomatesoignonsback.business.service.dto.AnimalDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@WebMvcTest(AnimalController.class)
+class AnimalControllerTest {
+
+    @Autowired
+    private AnimalController classUnderTest;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private AnimalService animalService;
+    private PageRequest defaultPageRequest;
+    @Captor
+    private ArgumentCaptor<PageRequest> captor;
+    private Slice<AnimalDto> slice;
+
+    @BeforeEach
+    public void setUp() {
+        defaultPageRequest = PageRequest.of(0, 20, Sort.Direction.ASC, "label");
+        slice = new SliceImpl<>(List.of(
+                new AnimalDto("Test"),
+                new AnimalDto("Ok")));
+    }
+
+    @Test
+    public void should_Call_Get_All_Animal_Dto_Of_Animal_Service() {
+        // given
+        when(animalService.getAllAnimalDto(defaultPageRequest)).thenReturn(slice);
+
+        // when
+        classUnderTest.getAllAnimalDto(defaultPageRequest);
+
+        // then
+        verify(animalService).getAllAnimalDto(defaultPageRequest);
+    }
+
+    @Test
+    public void pageRequest_Should_Be_First_Page_Of_20_Animals_Dto_Sorted_By_Asc_Label() throws Exception {
+        // given
+        when(animalService.getAllAnimalDto(defaultPageRequest)).thenReturn(slice);
+
+        // when
+        mockMvc.perform(get("/v1/animals"));
+
+        // then
+        verify(animalService).getAllAnimalDto(captor.capture());
+        PageRequest usedPageRequest = captor.getValue();
+        assertThat(usedPageRequest).isEqualTo(defaultPageRequest);
+    }
+
+    @Test
+    public void pageRequest_Should_Be_Second_Page_Of_Animals_Dto() throws Exception {
+        // given
+        PageRequest secondPage = PageRequest.of(1, 20, Sort.Direction.ASC, "label");
+        when(animalService.getAllAnimalDto(secondPage)).thenReturn(slice);
+
+        // when
+        mockMvc.perform(get("/v1/animals?page=1"));
+
+        // then
+        verify(animalService).getAllAnimalDto(captor.capture());
+        PageRequest usedPageRequest = captor.getValue();
+        assertThat(usedPageRequest).isEqualTo(secondPage);
+    }
+
+    @Test
+    public void pageRequest_Should_Be_Page_Of_5_Animals_Dto() throws Exception {
+        // given
+        PageRequest pageOf5AnimalDto = PageRequest.of(0, 5, Sort.Direction.ASC, "label");
+        when(animalService.getAllAnimalDto(pageOf5AnimalDto)).thenReturn(slice);
+
+        // when
+        mockMvc.perform(get("/v1/animals?size=5"));
+
+        // then
+        verify(animalService).getAllAnimalDto(captor.capture());
+        PageRequest usedPageRequest = captor.getValue();
+        assertThat(usedPageRequest).isEqualTo(pageOf5AnimalDto);
+    }
+
+    @Test
+    public void pageRequest_Should_Be_Page_Of_Animals_Dto_Sorted_By_Desc_Label() throws Exception {
+        // given
+        PageRequest pageSortedByDescLabel = PageRequest.of(0, 20, Sort.Direction.DESC, "label");
+        when(animalService.getAllAnimalDto(pageSortedByDescLabel)).thenReturn(slice);
+
+        // when
+        mockMvc.perform(get("/v1/animals?sort=label,desc"));
+
+        // then
+        verify(animalService).getAllAnimalDto(captor.capture());
+        PageRequest usedPageRequest = captor.getValue();
+        assertThat(usedPageRequest).isEqualTo(pageSortedByDescLabel);
+    }
+}
