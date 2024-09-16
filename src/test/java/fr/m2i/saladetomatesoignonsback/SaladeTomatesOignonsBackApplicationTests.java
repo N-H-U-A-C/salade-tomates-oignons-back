fb545f50303710441cc3407648dd8deb6f257346
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-09-16 11:22:14 +0200

    initial commit

---
+++ src/test/java/fr/m2i/saladetomatesoignonsback/SaladeTomatesOignonsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.m2i.saladetomatesoignonsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaladeTomatesOignonsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
