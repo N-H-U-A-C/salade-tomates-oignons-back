f4e733ab896e1a4881ec33fb9e1dc72d255fccd5
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-09-19 22:19:00 +0200

    0022/style: rename ingredient field by ingredientDto in AccountIngredientDto record

--- src/main/java/fr/m2i/saladetomatesoignonsback/business/service/mapper/AccountIngredientMapper.java
+++ src/main/java/fr/m2i/saladetomatesoignonsback/business/service/mapper/AccountIngredientMapper.java
@@ -3,6 +3,7 @@ package fr.m2i.saladetomatesoignonsback.business.service.mapper;
 import fr.m2i.saladetomatesoignonsback.business.domain.AccountIngredient;
 import fr.m2i.saladetomatesoignonsback.business.service.dto.AccountIngredientDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = IngredientMapper.class)
@@ -10,5 +11,6 @@ public interface AccountIngredientMapper {
 
     AccountIngredientMapper INSTANCE = Mappers.getMapper(AccountIngredientMapper.class);
 
+    @Mapping(target = "ingredientDto", source = "ingredient")
     AccountIngredientDto toAccountIngredientDto(AccountIngredient accountIngredient);
 }
