8c686166555667b2546c1119024867baab217ee3
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-09-20 22:55:56 +0200

    0024/feat: add findByUsernameAndPassword method in AccountRepository

--- src/main/java/fr/m2i/saladetomatesoignonsback/persistence/AccountRepository.java
+++ src/main/java/fr/m2i/saladetomatesoignonsback/persistence/AccountRepository.java
@@ -18,5 +18,4 @@ public interface AccountRepository extends CrudRepository<Account, UUID>, Paging
     int customDeleteById(UUID id);
 
     Optional<Account> findByUsernameAndPassword(String username, String password);
-
 }
