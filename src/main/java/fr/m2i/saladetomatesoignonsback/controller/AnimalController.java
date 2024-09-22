6e5afa433542b0e46beda21a932d2aa38d4deba7
N-H-U-A-C <presenteisme.pb@gmail.com>
2024-09-22 23:38:01 +0200

    0036/feat: config CORS for all controllers (except RecipeController)

--- src/main/java/fr/m2i/saladetomatesoignonsback/controller/AnimalController.java
+++ src/main/java/fr/m2i/saladetomatesoignonsback/controller/AnimalController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.util.Optional;
 import java.util.UUID;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping("/v1/animals")
 public class AnimalController {
